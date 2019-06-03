55d4f4e05e4e7d4e4197cbfea26459ec25191e29
indiff <indiff@qq.com>
2019-06-03 12:29:45 +0800

    update warning

--- Plugins/QTWindowManager/QTWindowManager.cs
+++ Plugins/QTWindowManager/QTWindowManager.cs
@@ -53,7 +53,7 @@ namespace QuizoPlugins {
         private const string CN_CabinetWClass = "CabinetWClass";
         private const string CN_Explorer = "";
 
-        private bool fFillAllScreen;
+        private bool fFillAllScreen = false ;
         private int windowColumnLength = 2;
         private int windowRowLength = 3;
 
