73258753c401c8cc81501e17fe4a3f9d6d317d1a
indiff <indiff@126.com>
2020-09-14 14:46:37 +0800

    🎞🎞🎞

--- 01build_release.bat
+++ 01build_release.bat
@@ -1,7 +1,7 @@
 @echo off
 set cur_path=%CD%
-call "C:\Visual Studio 2010 Ultimate\VC\vcvarsall.bat" x86
-:: call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+rem call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
 
 "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" "%cur_path%\QTTabBar Rebirth.sln" /t:Rebuild /property:Configuration=Release /property:Platform="Any CPU"
 pause
