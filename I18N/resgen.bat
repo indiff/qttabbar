be8298a0b0996d479b88feea741b058f55c4aac5
春风化雨细如丝 <indiff@126.com>
2022-10-20 11:14:53 +0800

    💔

--- I18N/resgen.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-"D:\Visual Studio 2010 Ultimate\Microsoft SDKs\Windows\v7.0A\bin\ResGen.exe" Lng_QTTabBar_pt_BR.txt "D:\svn\myqttabbar\QTTabBar\i18n\Resources_String_pt_BR.resx" /str:cs
-"D:\Visual Studio 2010 Ultimate\Microsoft SDKs\Windows\v7.0A\bin\ResGen.exe" Lng_QTTabBar_fr_FR.txt "D:\svn\myqttabbar\QTTabBar\i18n\Resources_String_fr_FR.resx" /str:cs
-"D:\Visual Studio 2010 Ultimate\Microsoft SDKs\Windows\v7.0A\bin\ResGen.exe" Lng_QTTabBar_tr_TR.txt "D:\svn\myqttabbar\QTTabBar\i18n\Resources_String_tr_TR.resx" /str:cs
-"D:\Visual Studio 2010 Ultimate\Microsoft SDKs\Windows\v7.0A\bin\ResGen.exe" Lng_QTTabBar_es_ES.txt "D:\svn\myqttabbar\QTTabBar\i18n\Resources_String_es_ES.resx" /str:cs
-
-pause
